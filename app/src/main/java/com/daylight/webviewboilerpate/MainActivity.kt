04a0cd35c340b92e0178a85ba3b99e98fe229025
ridwanmubarok <ridwanmubarok5@gmail.com>
2022-01-06 16:48:00 +0700

    Initial commit

---
+++ app/src/main/java/com/daylight/webviewboilerpate/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.daylight.webviewboilerpate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        webView.loadUrl("https://www.google.com/")
+    }
+}
